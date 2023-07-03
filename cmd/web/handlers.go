f832e7637d0214bc1d034bb74d208c3f613b3798
MohammadJavad14 <mrph14@gmail.com>
2023-07-03 22:01:03 +0330

    writing a stub handler for choosing a plan

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -149,15 +149,26 @@ func (app *Config) ActivateAccount(w http.ResponseWriter, r *http.Request) {
 
 	app.Session.Put(r.Context(), "flash", "Account activated, You can now log in.")
 	http.Redirect(w, r, "/login", http.StatusSeeOther)
+}
+
+func (app *Config) SubscribeToPlan(w http.ResponseWriter, r *http.Request) {
+	// get the id of the plan that is chosen
+
+	// get the plan from the database
+
+	// get the user from the session
 
 	// generate an invoice
 
-	// send an email with attachments
+	// send an email with the invoice attached
 
-	// sen an email with the invoice attached
+	// generate a manual
 
-	// subscribe the use to an account
+	// send an email with the manual attached
 
+	// subscribe the user to an account
+
+	// redirect
 }
 
 func (app *Config) ChooseSubscription(w http.ResponseWriter, r *http.Request) {
